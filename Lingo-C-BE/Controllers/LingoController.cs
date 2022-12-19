56da37557da5e7935957cf8542686298e2c2591e
Djarid89 <56673937+Djarid89@users.noreply.github.com>
2022-12-19 16:03:22 +0100

    Dictionary update

--- Lingo-C-BE/Controllers/LingoController.cs
+++ Lingo-C-BE/Controllers/LingoController.cs
@@ -12,6 +12,16 @@ namespace Lingo_C_BE.Controllers
     public bool Valid { get; set; }
   }
 
+  public class UpdateWordsData
+  {
+    public List<string> Words { get; set; }
+  }
+
+  public class UpdateWordsResult
+  {
+    public bool Valid { get; set; }
+  }
+
   [ApiController]
   [Route("lingo/")]
   public class LingoController : ControllerBase
@@ -29,8 +39,7 @@ namespace Lingo_C_BE.Controllers
         }
         result.Word = words.ElementAt(new Random().Next(words.Count - 1));
       }
-      
-
+        
       return result;
     }
 
@@ -44,6 +53,34 @@ namespace Lingo_C_BE.Controllers
         result.Valid = words.Exists(w => w.ToUpper().Equals(word.ToUpper()));
       }
 
+      return result;
+    }
+
+    [HttpPost("updatewords")]
+    public UpdateWordsResult UpdateWords(UpdateWordsData data)
+    {
+      UpdateWordsResult result = new UpdateWordsResult();
+      string path = @".\assets\lingoWords.txt";
+      FileStream fs = System.IO.File.Open(path, FileMode.Open, FileAccess.Read);
+      List<string> words = new List<string>();
+      using(StreamReader sr = new StreamReader(fs))
+      {
+        words = new List<string>(sr.ReadToEnd().Split(','));
+        fs.Dispose();
+      }
+
+      using(StreamWriter sw = System.IO.File.AppendText(path))
+      {
+        data.Words.ForEach(word =>
+        {
+          if(!words.Exists(w => w.ToUpper().Equals(word.ToUpper())) && CheckWord(word).Valid)
+          {
+            sw.Write(',' + word);
+          }
+        });
+      }
+
+      result.Valid = true;
       return result;
     }
   }
